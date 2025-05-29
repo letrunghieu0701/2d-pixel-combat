dd44295e68f04eb2251afd98a7b19011ad05814c
Lê Trung Hiếu <hieuletrung0701@gmail.com>
2025-05-29 18:56:57 +0700

    Add player animations (idle, running). Adjust facing direction based on mouse and player position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,15 @@ public class PlayerController : MonoBehaviour
     private PlayerControllers _playerControllers;
     private Vector2 _movementInput;
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _playerControllers = new PlayerControllers();
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
@@ -28,6 +33,7 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        AdjustFacingDirection();
         Move();
     }
 
@@ -39,11 +45,20 @@ public class PlayerController : MonoBehaviour
     private void ReadPlayerInput()
     {
         _movementInput = _playerControllers.Movement.Move.ReadValue<Vector2>();
-        Debug.Log(_movementInput.x + " " + _movementInput.y);
+
+        _animator.SetFloat("moveX", _movementInput.x);
+        _animator.SetFloat("moveY", _movementInput.y);
     }
 
     private void Move()
     {
         _rigidbody.MovePosition(_rigidbody.position + _movementInput * (_moveSpeed * Time.deltaTime));
+    }
+
+    private void AdjustFacingDirection()
+    {
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(_rigidbody.position);
+        bool isFlipLeftSide = Input.mousePosition.x < playerScreenPoint.x;
+        _spriteRenderer.flipX = isFlipLeftSide;
     }
 }
