17c914b6641628582b7d3f616a07b39d58f355b1
Lê Trung Hiếu <hieuletrung0701@gmail.com>
2025-05-31 00:30:37 +0700

    Add random roaming enemy AI

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum EnemyState
+    {
+        Roaming,
+    }
+
+    private EnemyState _state;
+    private EnemyMovement _movement;
+    private float _moveSpeed;
+
+    private void Awake()
+    {
+        _state = EnemyState.Roaming;
+        _movement = GetComponent<EnemyMovement>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (_state == EnemyState.Roaming)
+        {
+            Vector2 roamPosition = GetRoamingPosition();
+            Debug.Log(roamPosition);
+            _movement.SetMoveDirection(roamPosition);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f, 1f)).normalized;
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 1.5f;
+    private Vector2 _moveDirection;
+    private Rigidbody2D _rigidBody;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidBody.MovePosition(_rigidBody.position + _moveDirection * (_moveSpeed * Time.fixedDeltaTime));
+    }
+
+    public void SetMoveDirection(Vector2 moveDirection)
+    {
+        _moveDirection = moveDirection;
+    }
+}
