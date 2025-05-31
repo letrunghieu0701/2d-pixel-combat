2b41d4250048c04f03cabc51ba65da56b0f12a18
Lê Trung Hiếu <hieuletrung0701@gmail.com>
2025-05-31 18:46:01 +0700

    Sword: change facing direction based on mouse and player position

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class Sword : MonoBehaviour
 {
-    private PlayerControllers _playerController;
+    private PlayerControllers _playerControllers;
     private Animator _animator;
+    private PlayerController _playerController;
+    private ActiveWeapon _activeWeapon;
 
     private void Awake()
     {
-        _playerController = new PlayerControllers();
+        _playerControllers = new PlayerControllers();
         _animator = GetComponent<Animator>();
+        _playerController = GetComponentInParent<PlayerController>();
+        _activeWeapon = GetComponentInParent<ActiveWeapon>();
     }
 
     private void Start()
     {
-        _playerController.Combat.Attack.started += _ => Attack();
+        _playerControllers.Combat.Attack.started += _ => Attack();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustFacingDirection();
     }
 
     private void OnEnable()
     {
-        _playerController.Enable();
+        _playerControllers.Enable();
     }
 
     private void Attack()
     {
         _animator.SetTrigger("Attack");
+    }
+
+    private void AdjustFacingDirection()
+    {
+
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(_playerController.transform.position);
+        bool isFlipLeftSide = Input.mousePosition.x < playerScreenPoint.x;
+        if (isFlipLeftSide)
+        {
+            _activeWeapon.transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else
+        {
+            _activeWeapon.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
