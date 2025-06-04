148343e7fe80f9223940438315014150bc819365
Lê Trung Hiếu <hieuletrung0701@gmail.com>
2025-06-04 22:47:13 +0700

    Add dashing for player

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,13 +7,20 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController Instance;
 
-    [SerializeField] private float _moveSpeed = 1f;
+    [SerializeField] private float _moveSpeed = 1;
+    private float _defaultMoveSpeed;
+
+    [SerializeField] private float _dashSpeed = 2;
+    [SerializeField] private float _dashTime = 0.2f;
+    [SerializeField] private float _dashCoolDownTime = 2;
+    private bool _canDash = true;
 
     private PlayerControllers _playerControllers;
     private Vector2 _movementInput;
     private Rigidbody2D _rigidbody;
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
+    private TrailRenderer _trailrenderer;
 
     private bool _isFacingLeft;
     public bool IsFacingLeft
@@ -27,11 +34,14 @@ public class PlayerController : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _trailrenderer = transform.Find("TrailDash").GetComponent<TrailRenderer>();
+        _trailrenderer.emitting = false;
+        _defaultMoveSpeed = _moveSpeed;
     }
 
     private void Start()
     {
-        
+        _playerControllers.Combat.Dash.performed += _ => Dash();
     }
 
     private void Update()
@@ -68,5 +78,28 @@ public class PlayerController : MonoBehaviour
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(_rigidbody.position);
         _isFacingLeft = Input.mousePosition.x < playerScreenPoint.x;
         _spriteRenderer.flipX = _isFacingLeft;
+    }
+
+    private void Dash()
+    {
+        if (!_canDash)
+        {
+            return;
+        }
+
+        _canDash = false;
+        _moveSpeed = _dashSpeed;
+        _trailrenderer.emitting = true;
+        StartCoroutine(EndDashRoutine());
+    }
+
+    private IEnumerator EndDashRoutine()
+    {
+        yield return new WaitForSeconds(_dashTime);
+        _moveSpeed = _defaultMoveSpeed;
+        _trailrenderer.emitting = false;
+
+        yield return new WaitForSeconds(_dashCoolDownTime);
+        _canDash = true;
     }
 }
